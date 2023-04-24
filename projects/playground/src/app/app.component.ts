d158030e1338401bbbb276d1d40064e3490771c2
Aurelien <linux.aurelien@gmail.com>
2023-04-24 11:44:42 +0200

    ajout de validateur personnalisé

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -7,11 +7,19 @@ import { NgForm } from '@angular/forms';
     <h1>Angular Avancé !</h1>
     <pre>{{ form.value | json }}</pre>
     Formulaire Valide : {{ form.valid }}
-    <form #form="ngForm" (submit)="onSubmit(form)">
+    <form
+      ngForm
+      #form="ngForm"
+      [ngFormOptions]="{ updateOn: 'change' }"
+      (submit)="onSubmit(form)"
+    >
       <input
         required
+        bannedEmail="test@test.com"
+        uniqueEmail
         email
         ngModel
+        [ngModelOptions]="{ updateOn: 'change' }"
         #email="ngModel"
         [class.is-invalid]="email.touched && email.invalid"
         [class.is-valid]="email.touched && email.valid"
@@ -21,6 +29,16 @@ import { NgForm } from '@angular/forms';
         class="form-control mb-2"
         placeholder="Votre adresse email"
       />
+      <p class="text-info" *ngIf="email.pending">
+        <span class="spinner-border spinner-border-sm"></span>
+        Vérification en Cours
+      </p>
+      <p
+        class="invalid-feedback"
+        *ngIf="email.touched && email.hasError('bannedEmail')"
+      >
+        L'adresse email {{ email.getError('bannedEmail') }} est interdite
+      </p>
       <p
         class="invalid-feedback"
         *ngIf="email.touched && email.hasError('required')"
@@ -33,56 +51,64 @@ import { NgForm } from '@angular/forms';
       >
         L'adresse email est invalide
       </p>
-      <input
-        required
-        minlength="3"
-        ngModel
-        [class.is-invalid]="password.touched && password.invalid"
-        [class.is-valid]="password.touched && password.valid"
-        #password="ngModel"
-        type="password"
-        name="password"
-        id="password"
-        class="form-control mb-2"
-        placeholder="Mot de passe"
-      />
-      <p
-        class="invalid-feedback"
-        *ngIf="password.touched && password.hasError('required')"
-      >
-        Le mot de passe est Obligatoire
-      </p>
-      <p
-        class="invalid-feedback"
-        *ngIf="password.touched && password.hasError('minlength')"
-      >
-        Le mot de passe doit faire plus de 3 caractères
-      </p>
-      <input
-        required
-        minlength="3"
-        ngModel
-        [class.is-invalid]="confirm.touched && confirm.invalid"
-        [class.is-valid]="confirm.touched && confirm.valid"
-        #confirm="ngModel"
-        type="password"
-        name="confirm"
-        id="confirm"
-        class="form-control mb-2"
-        placeholder="Confirmation du mot de passe"
-      />
-      <p
-        class="invalid-feedback"
-        *ngIf="confirm.touched && confirm.hasError('required')"
-      >
-        La confirmation est Obligatoire
-      </p>
-      <p
-        class="invalid-feedback"
-        *ngIf="confirm.touched && confirm.hasError('minlength')"
-      >
-        La confirmation doit faire plus de 3 caractères
-      </p>
+      <div ngModelGroup="security" confirmPassword>
+        <input
+          required
+          minlength="3"
+          ngModel
+          [class.is-invalid]="password.touched && password.invalid"
+          [class.is-valid]="password.touched && password.valid"
+          #password="ngModel"
+          type="password"
+          name="password"
+          id="password"
+          class="form-control mb-2"
+          placeholder="Mot de passe"
+        />
+        <p
+          class="invalid-feedback"
+          *ngIf="password.touched && password.hasError('required')"
+        >
+          Le mot de passe est Obligatoire
+        </p>
+        <p
+          class="invalid-feedback"
+          *ngIf="password.touched && password.hasError('minlength')"
+        >
+          Le mot de passe doit faire plus de 3 caractères
+        </p>
+        <input
+          required
+          minlength="3"
+          ngModel
+          [class.is-invalid]="confirm.touched && confirm.invalid"
+          [class.is-valid]="confirm.touched && confirm.valid"
+          #confirm="ngModel"
+          type="password"
+          name="confirm"
+          id="confirm"
+          class="form-control mb-2"
+          placeholder="Confirmation du mot de passe"
+        />
+        <p
+          class="invalid-feedback"
+          *ngIf="confirm.touched && confirm.hasError('required')"
+        >
+          La confirmation est Obligatoire
+        </p>
+        <p
+          class="invalid-feedback"
+          *ngIf="confirm.touched && confirm.hasError('minlength')"
+        >
+          La confirmation doit faire plus de 3 caractères
+        </p>
+        <p
+          class="invalid-feedback"
+          *ngIf="confirm.touched && confirm.hasError('confirmPassword')"
+        >
+          La confirmation ne correspond pas au mot de passe
+        </p>
+      </div>
       <button class="btn btn-success" [disabled]="form.invalid">
         Inscription
       </button>
--- projects/playground/src/app/app.module.ts
+++ projects/playground/src/app/app.module.ts
@@ -3,9 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BannedEmailDirective } from './banned-email.directive';
+import { UniqueEmailValidator } from './unique-email.directive';
+import { ConfirmPasswordValidator } from './confirm-password.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    BannedEmailDirective,
+    UniqueEmailValidator,
+    ConfirmPasswordValidator,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
