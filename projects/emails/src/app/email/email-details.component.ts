46d9d36c15cb9c6bed2e8463484d13b154cda3e2
Aurelien <linux.aurelien@gmail.com>
2023-07-24 12:35:17 +0200

    fin sur le routing de base

--- projects/emails/src/app/email/email-details.component.ts
+++ projects/emails/src/app/email/email-details.component.ts
@@ -1,47 +1,61 @@
 import { Component } from '@angular/core';
 import { Email } from './types';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FAKE_EMAILS_DATA } from '../data';
+import { Observable, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-email-details',
   template: `
-    <h1>{{ email?.subject }}</h1>
-    <div class="d-flex justify-content-between align-items-center">
-      <em
-        >{{ email?.contactName }} ({{ email?.from }}), le {{ email?.date }}</em
+    <ng-template #fallback>
+      <h3>Une erreur est survenue, aucun email trouvé</h3>
+      <a [routerLink]="['../../']" class="btn btn-primary"
+        >Retour à la boite de reception</a
       >
-      <button class="btn btn-danger">Supprimer</button>
+    </ng-template>
+    <div *ngIf="email$ | async as email; else fallback">
+      <h1>{{ email.subject }}</h1>
+      <div class="d-flex justify-content-between align-items-center">
+        <em>{{ email.contactName }} ({{ email.from }}), le {{ email.date }}</em>
+        <button class="btn btn-danger" (click)="deleteEmail()">
+          Supprimer
+        </button>
+      </div>
+      <hr />
+      <p *ngFor="let part of getBodyParts(email.body)">
+        {{ part }}
+      </p>
+      <nav>
+        <a [routerLink]="['../', email.id - 1]" class="btn btn-secondary"
+          >&lt; Mail précédent</a
+        ><a [routerLink]="['../', email.id + 1]" class="btn btn-secondary"
+          >Mail suivant &gt;</a
+        >
+      </nav>
     </div>
-    <hr />
-    <p *ngFor="let part of bodyParts">
-      {{ part }}
-    </p>
-    <nav>
-      <a href="#" class="btn btn-secondary">&lt; Mail précédent</a
-      ><a href="#" class="btn btn-secondary">Mail suivant &gt;</a>
-    </nav>
   `,
   styles: [],
 })
 export class EmailDetailsComponent {
-  email?: Email;
+  email$?: Observable<Email>;
 
-  get bodyParts() {
-    return this.email?.body.split('\r\n');
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  deleteEmail() {
+    this.router.navigate(['../../'], {
+      relativeTo: this.route,
+    });
   }
 
-  constructor(private route: ActivatedRoute) {}
+  getBodyParts(body: string) {
+    return body.split('\r\n');
+  }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((paramMap) => {
-      if (!paramMap.has('id')) {
-        return;
-      }
-
-      const id = +paramMap.get('id')!;
-
-      this.email = FAKE_EMAILS_DATA.find((email) => email.id === id) as Email;
-    });
+    this.email$ = this.route.paramMap.pipe(
+      filter((paramMap) => paramMap.has('id')),
+      map((paramMap) => +paramMap.get('id')!),
+      map((id) => FAKE_EMAILS_DATA.find((email) => email.id === id) as Email)
+    );
   }
 }
--- projects/emails/src/app/email/emails-list.component.ts
+++ projects/emails/src/app/email/emails-list.component.ts
@@ -2,27 +2,30 @@ import { Component } from '@angular/core';
 import { FAKE_EMAILS_DATA } from '../data';
 import { Email } from './types';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-emails-list',
   template: `
-    <h1>{{ title }}</h1>
-    <table class="table">
-      <tbody>
-        <tr
-          (click)="goToEmail(email.id)"
-          [class.fw-bold]="!email.read"
-          *ngFor="let email of emails"
-        >
-          <td>{{ email.contactName }}</td>
-          <td class="w-50">{{ email.subject }}</td>
-          <td>{{ email.date }}</td>
-          <td>
-            <button class="btn btn-sm btn-danger">Supprimer</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div *ngIf="emailAndTiltle$ | async as vm">
+      <h1>{{ vm.title }}</h1>
+      <table class="table">
+        <tbody>
+          <tr
+            (click)="goToEmail(email.id)"
+            [class.fw-bold]="!email.read"
+            *ngFor="let email of vm.emails"
+          >
+            <td>{{ email.contactName }}</td>
+            <td class="w-50">{{ email.subject }}</td>
+            <td>{{ email.date }}</td>
+            <td>
+              <button class="btn btn-sm btn-danger">Supprimer</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   `,
   styles: [
     `
@@ -37,8 +40,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   ],
 })
 export class EmailsListComponent {
-  emails: Email[] = [];
-  title: string = 'Boite de Reception';
+  emailAndTiltle$?: Observable<{ emails: Email[]; title: string }>;
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
@@ -47,28 +49,25 @@ export class EmailsListComponent {
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((paramMap) => {
-      const type = paramMap.get('type');
-      //console.log(type);
+    this.emailAndTiltle$ = this.route.paramMap.pipe(
+      map((paramMap) => paramMap.get('type')),
+      map((type) => {
+        if (!type) {
+          return {
+            emails: (FAKE_EMAILS_DATA as Email[]).filter(
+              (email) => email.status === 'INBOX'
+            ),
+            title: 'Boite de Reception',
+          };
+        }
 
-      if (!type) {
-        this.emails = (FAKE_EMAILS_DATA as Email[]).filter(
-          (email) => email.status === 'INBOX'
-        );
-        this.title = 'Boite de Reception';
-        return;
-      }
-
-      this.emails = (FAKE_EMAILS_DATA as Email[]).filter(
-        (email) => email.status === type?.toUpperCase()
-      );
-
-      if (type === 'sent') {
-        this.title = 'Emails Envoyé';
-        return;
-      }
-
-      this.title = 'Corbeille';
-    });
+        return {
+          emails: (FAKE_EMAILS_DATA as Email[]).filter(
+            (email) => email.status === type?.toUpperCase()
+          ),
+          title: type === 'sent' ? 'Emails envoyé' : 'Corbeille',
+        };
+      })
+    );
   }
 }
